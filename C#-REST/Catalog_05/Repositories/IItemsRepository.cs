799e075a2a7732fc6bb73406bb45b113eeaf6bf3
Brian Su <briansu2004@hotmail.com>
2022-03-05 18:19:28 -0500

    05

---
+++ C#-REST/Catalog_05/Repositories/IItemsRepository.cs
@@ -0,0 +1,22 @@
+
+
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Item GetItem(Guid id);
+
+        IEnumerable<Item> GetItems();
+
+        void CreateItem(Item item);
+
+        void UpdateItem(Item item);
+
+        void DeleteItem(Guid id);
+    }
+}
